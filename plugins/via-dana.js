4c3c597c3cbd8de36ddd60ef6af40bee8fc956bb
D A M I A N                     ⭐ <109391741+DamianBotz@users.noreply.github.com>
2022-07-18 13:25:38 +0700

    Update via-dana.js

--- plugins/via-dana.js
+++ plugins/via-dana.js
@@ -5,16 +5,16 @@ Hay👋‹, ingin Donasi?
 
 💌 *Pembayaran*
 💳 Via: Dana 
-📞 Nomor: 088215689772
-👤 A/n: Fangz
-📝 Mitra: Fangz BOT
+📞 Nomor: 085785845416
+👤 A/n: Damian
+📝 Mitra: Damian BOT
 💰 Metode pembayaran: Online ( ~Cod~ )
 
 
 ®KLIK *SUDAH BAYAR* JIKA SUDAH TRANSFER 
 `
 const templateButtons = [
-    {index: 1, urlButton: {displayText: 'QRIS', url: 'https://telegra.ph/file/ff71853efeb006d635a90.jpg'}},
+    {index: 1, urlButton: {displayText: 'QRIS', url: 'https://telegra.ph/file/199b3e69af1419c49af96.jpg'}},
     {index: 4, quickReplyButton: {displayText: 'Sudah membayar', id: '.sudahbayar'}},
 ]
 let tm = {
